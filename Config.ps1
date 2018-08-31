f7b3d565ba9481bf2befc34c145578824d924bbc
Blaubund <blaubund@users.noreply.github.com>
2018-08-31 10:31:44 +0200

    Added default locations for Center and Aberration

--- Config.ps1
+++ Config.ps1
@@ -9,8 +9,10 @@ $defaultDestinationFolder = "Wild"
 $savedFolder = "D:\SteamLibrary\steamapps\common\ARK\ShooterGame\Saved\"
 
 $defaultSaveFiles = @{
-    "Island" =    "$($savedFolder)SavedArksLocal\TheIsland.ark"
-    "SE" =        "$($savedFolder)ScorchedEarth_PSavedArksLocal\ScorchedEarth_P.ark"
-    "Ragnarok" =  "$($savedFolder)RagnarokSavedArksLocal\Ragnarok.ark"
+    "Island" =      "$($savedFolder)SavedArksLocal\TheIsland.ark"
+    "SE" =          "$($savedFolder)ScorchedEarth_PSavedArksLocal\ScorchedEarth_P.ark"
+    "Ragnarok" =    "$($savedFolder)RagnarokSavedArksLocal\Ragnarok.ark"
+    "Aberration" =  "$($savedFolder)Aberration_PSavedArksLocal\Aberration_P.ark"
+    "Center" =      "$($savedFolder)TheCenterSavedArksLocal\TheCenter.ark"
 }
 
